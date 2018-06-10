7ecd464d09ec9a060173aa71409a2305b65044fd
Haroldo Olivieri <haroldo@carioca.build>
2018-06-10 06:09:43 +0200

    name in portuguese

--- app/src/main/kotlin/haroldolivieri/currencyexchanger/view/ViewExtensions.kt
+++ app/src/main/kotlin/haroldolivieri/currencyexchanger/view/ViewExtensions.kt
@@ -92,14 +92,13 @@ fun EditText.resetLimitLength() {
     filters = emptyArray()
 }
 
-
 class DecimalDigitsInputFilter(digitsAfterZero: Int) : InputFilter {
-    private var mPattern: Pattern = Pattern.compile("[0-9]+((\\.[0-9]{0," + (digitsAfterZero - 1) + "})?)||(\\.)?")
+    private var mPattern: Pattern = Pattern
+            .compile("[0-9]+((\\.[0-9]{0," + (digitsAfterZero - 1) + "})?)||(\\.)?")
     override fun filter(source: CharSequence,
                         start: Int, end: Int,
                         dest: Spanned, dstart: Int, dend: Int): CharSequence? {
         val matcher = mPattern.matcher(dest)
         return if (!matcher.matches()) "" else null
     }
-
 }
